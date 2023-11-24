b444e3395ec8146ec8672174254946b754587709
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-11-24 07:18:11 +0600

    python first app succesfuly run in local and virtual Platform

--- python-fast-app/shells/install.sh
+++ python-fast-app/shells/install.sh
@@ -3,10 +3,12 @@ sudo apt update
 sudo apt install -y python3-pip python3-venv
 
 # Create and activate the virtual environment
-# python3 -m venv venv
-# virtualenv venv
-# venv/Scripts/activate.bat
-# source venv/bin/activate
+python3 -m venv venv
+venv/Scripts/activate.bat
 
 # Install dependencies
-pip install -r /home/ubuntu/requirements.txt
+pip install -r requirements.txt
+
+# Manage the database port and host in the .env file (optional)
+# Run the application
+python app.py
